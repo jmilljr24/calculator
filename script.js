937507e6029142a3f5dc82100267412264450e48
jmilljr24 <jmilljr24@gmail.com>
2022-12-01 13:01:17 -0500

    Fix layout of calculator and query button numbers

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("test");
+let currentNumber = 0;
+let previousNumber = 0;
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const numberButton = document.querySelectorAll("[data-number");
+numberButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+    return button;
+  });
+});
